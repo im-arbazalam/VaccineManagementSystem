76bc2d24e70d341f7a0a7380ec45762ce408cd7d
Arbaz Alam <arbazbinirfan@gmail.com>
2023-11-30 21:52:30 +0530

    add aome changes on Transformer

--- src/main/java/com/example/VaccineManagementSystem/Transformer/UserTransformer.java
+++ src/main/java/com/example/VaccineManagementSystem/Transformer/UserTransformer.java
@@ -7,16 +7,18 @@ import com.example.VaccineManagementSystem.ResponseDtos.UserResponseDto;
 public class UserTransformer {
     public static User ConvertDtoToEntity (AddUserDto addUserDto){
         User user= User.builder()
-                .emailId(addUserDto.getEmailId()).name(addUserDto.getName())
-                .age(addUserDto.getAge()).gender(addUserDto.getGender())
+                .emailId(addUserDto.getEmailId())
+                .name(addUserDto.getName())
+                .age(addUserDto.getAge())
+                .gender(addUserDto.getGender())
                 .mobileNo(addUserDto.getMobileNo())
                 .build();
         return user;
     }
 
-    public static UserResponseDto ConvertEntityToEntity(User user){
-        UserResponseDto userResponseDto=UserResponseDto.builder().
-                gender(user.getGender())
+    public static UserResponseDto ConvertEntityToDto(User user){
+        UserResponseDto userResponseDto=UserResponseDto.builder()
+                .gender(user.getGender())
                 .age(user.getAge())
                 .name(user.getName())
                 .mobileNo(user.getMobileNo())
